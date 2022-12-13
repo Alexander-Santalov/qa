fe3a8317cc967511d1dc508e615e29d909662e17
Alexander Santalov <vgtrk520@yandex.ru>
2022-12-13 22:41:44 +0300

    send_homework_fix

--- tests/test_github.py
+++ tests/test_github.py
@@ -16,15 +16,11 @@ from allure_commons.types import AttachmentType
 def test_github():
     browser.open("https://github.com")
     browser.driver.maximize_window()
-
     s(".header-search-input").click()
     s(".header-search-input").send_keys("eroshenkoam/allure-example")
     s(".header-search-input").submit()
-
     s(by.link_text("eroshenkoam/allure-example")).click()
-
     s("#issues-tab").click()
-
     s(by.partial_text("#76")).should(be.visible)
 
 
@@ -37,18 +33,14 @@ def test_github():
 def test_dynamic_steps():
     with allure.step("Открываем главную страницу"):
         browser.open("https://github.com")
-
     with allure.step("Ищем репозитория"):
         s(".header-search-input").click()
         s(".header-search-input").send_keys("eroshenkoam/allure-example")
         s(".header-search-input").submit()
-
     with allure.step("Переходим по ссылке репозитория"):
         s(by.link_text("eroshenkoam/allure-example")).click()
-
     with allure.step("Открываем таб Issues"):
         s("#issues-tab").click()
-
     with allure.step("Проверяем наличие Issue с номером 76"):
         s(by.partial_text("#76")).should(be.visible)
     with allure.step("Делаем финальный скриншот"):
